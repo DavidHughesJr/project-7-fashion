ec1d8d7a065555a5e6948b422d5202423daaf5fe
DavidHughesJr <hughes014@yahoo.com>
2023-04-23 21:00:00 -0500

    update

--- src/pages/Root.js
+++ src/pages/Root.js
@@ -1,6 +1,5 @@
-import { useState, useEffect } from 'react';
 import { Outlet } from "react-router-dom";
-import { ThemeProvider, Box } from '@mui/material';
+import { ThemeProvider } from '@mui/material';
 import theme from '../theme';
 import NavBar from "../components/common/NavBar"
 import Footer from "../components/common/Footer"
@@ -8,23 +7,11 @@ import Footer from "../components/common/Footer"
 
 const Root = () => {
 
-    const loadingTime = 0
-    const [loading, setLoading] = useState(true)
-
-    useEffect(() => {
-        setTimeout(() => {
-            setLoading(!loading)
-        }, loadingTime)
-        // eslint-disable-next-line
-    }, [])
-
     return (
         <ThemeProvider theme={theme} >
-            <Box sx={{ opacity: loading ? 0 : 100, touchAction: loading ? 'none' : 'auto' }}>
-                <NavBar />
-                <Outlet />
-                <Footer />
-            </Box>
+            <NavBar />
+            <Outlet />
+            <Footer />
         </ThemeProvider >
     )
 }
